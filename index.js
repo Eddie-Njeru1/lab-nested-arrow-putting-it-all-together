eb8bb38429e7f2f8d0e1df8c0add28c2445c4f61
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-04-19 21:34:32 +0300

    "initial commit"

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// Create outer function and initialise login tracking
+function createLoginTracker (userInfo) {
+  const username = {
+    "username": "user"
+  };
+  const password = {
+    "password": "password123"
+  };
+  let attemptCount = 0;
+    
 
-
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+  }
