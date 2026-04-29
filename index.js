5bce68527d6e8bc16c17456a210e3237272de025
Eddie Nyaga Njeru <eddie.njeru@student.moringaschool.com>
2026-04-29 20:55:53 +0300

    "First Attempt"

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
-// Create outer function and initialise login tracking
-function createLoginTracker (userInfo) {
-  const username = {
-    "username": "user"
-  };
-  const password = {
-    "password": "password123"
-  };
-  let attemptCount = 0;
-    
+//Readline to initiate user interaction
+const readline = require("readline");
 
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const userInfo = {
+    username: "",
+    password: "",
+  };
+
+// Create outer function with username and password object parameters
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+    return (passwordAttempt) => { //Arrow function for handling each login attempt count
+      attemptCount++; 
+
+      //Login Attempt Variable to 
+      if (attemptCount > 3) {
+        return ("Account locked due to too many failed login attempts!");
+      }
+
+      if (passwordAttempt === userInfo.password) {
+        return ("Login Successful!");
+      } else {
+        return ("Login Failed!");
+      }
+  
+  
+
+  const login = createLoginTracker(userInfo);
+  console.log("Welcome! You have 3 login attempt.");
+
+  //Function to ask for password
+  const askForPassword = () =>
+    rl.question("Enter password:" (passwordAttempt)) => {
+      const result = login(passwordAttempt);
+      console.log(result);
+
+      
+    }
+      
+    }
+
+}
+  
